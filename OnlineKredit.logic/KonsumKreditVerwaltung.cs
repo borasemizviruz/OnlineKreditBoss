8003763061601d8a166379c3361517a370cb399f
borasemiz <borasemiz@live.at>
2016-11-24 13:35:01 +0100

    BL.done

--- OnlineKredit.logic/KonsumKreditVerwaltung.cs
+++ OnlineKredit.logic/KonsumKreditVerwaltung.cs
@@ -49,7 +49,397 @@ namespace OnlineKredit.logic
             Debug.WriteLine("KonsumkreditVerwaltun g - KreditRahmenSpeichern");
             Debug.Indent();
 
-            bool;
+            bool erfolgreich = false;
+
+            try
+            {
+                using (var context = new dbOnlineKreditLAPEntities1())
+                {
+                    Kunde aktKunde = context.AlleKunden.Where(x => x.ID == idKunde).FirstOrDefault();
+
+                    if (aktKunde != null)
+                    {
+                        KreditWunsch neuerKreditWunsch = new KreditWunsch()
+                        {
+                            Betrag = (decimal)kreditBetrag,
+                            Laufzeit = laufzeit,
+                            ID = idKunde
+                        };
+
+                        context.AlleKreditWünsche.Add(neuerKreditWunsch);
+                    }
+
+                    int anzahlZeilenBetroffen = context.SaveChanges();
+                    erfolgreich = anzahlZeilenBetroffen >= 1;
+                    Debug.WriteLine($"{anzahlZeilenBetroffen} KreditRahmen gespeichert!");
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("Fehler in KreditRahmenSpeichern");
+                Debug.Indent();
+                Debug.WriteLine(ex.Message);
+                Debug.Unindent();
+                Debugger.Break();
+            }
+
+            Debug.Unindent();
+            return erfolgreich;
+        }
+
+        public static bool FinanzielleSituationSpeichern(double nettoEinkommen, double ratenVerpflichtungen, double wohnkosten, double einkünfteAlimenteUnterhalt, double unterhaltsZahlungen, int idKunde)
+        {
+            Debug.WriteLine("KonsumKreditVerwaltung - FinanzielleSituationSpeichern");
+            Debug.Indent();
+
+            bool erfolgreich = false;
+
+            try
+            {
+                using (var context = new dbOnlineKreditLAPEntities1())
+                {
+
+                    Kunde aktKunde = context.AlleKunden.Where(x => x.ID == idKunde).FirstOrDefault();
+
+                    if (aktKunde != null)
+                    {
+                        FinanzielleSituation neueFinanzielleSituation = new FinanzielleSituation()
+                        {
+                            MonatsEinkommen = (decimal)nettoEinkommen,
+                            AusgabenALIUNT = (decimal)unterhaltsZahlungen,
+                            EinkuenfteAlimenteUnterhalt = (decimal)einkünfteAlimenteUnterhalt,
+                            Wohnkosten = (decimal)wohnkosten,
+                            RatenZahlungen = (decimal)ratenVerpflichtungen,
+                            ID = idKunde
+                        };
+
+                        context.AlleFinanzielleSituationen.Add(neueFinanzielleSituation);
+                    }
+
+                    int anzahlZeilenBetroffen = context.SaveChanges();
+                    erfolgreich = anzahlZeilenBetroffen >= 1;
+                    Debug.WriteLine($"{anzahlZeilenBetroffen} FinanzielleSituation gespeichert!");
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("Fehler in FinanzielleSituation");
+                Debug.Indent();
+                Debug.WriteLine(ex.Message);
+                Debug.Unindent();
+                Debugger.Break();      
+            }
+
+            Debug.Unindent();
+            return erfolgreich;
+        }
+
+        public static List<Branche> BranchenLaden()
+        {
+            Debug.WriteLine("KonsumKreditVerwaltung - BranchenLaden");
+            Debug.Indent();
+
+            List<Branche> alleBranchen = null;
+
+            try
+            {
+                using (var context = new dbOnlineKreditLAPEntities1())
+                {
+                    alleBranchen = context.AlleBranchen.ToList();
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("Fehler in BranchenLaden");
+                Debug.Indent();
+                Debug.WriteLine(ex.Message);
+                Debug.Unindent();
+                Debugger.Break();
+            }
+
+            Debug.Unindent();
+            return alleBranchen;
+        }
+
+        public static List<Beschaeftigungsart> BeschaeftigungsArtenLaden()
+        {
+            Debug.WriteLine("KonsumKreditVerwaltung - Beschaeftigungsart");
+            Debug.Indent();
+
+            List<Beschaeftigungsart> alleBeschaeftigungsArten = null;
+
+            try
+            {
+                using (var context = new dbOnlineKreditLAPEntities1())
+                {
+                    alleBeschaeftigungsArten = context.AlleBeschaeftigungsarten.ToList();
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("Fehler in Beschaeftigungsart");
+                Debug.Indent();
+                Debug.WriteLine(ex.Message);
+                Debug.Unindent();
+                Debugger.Break();
+            }
+
+            Debug.Unindent();
+            return alleBeschaeftigungsArten;
+        }
+
+        public static List<Schulabschluss> BildungsAngabenLaden()
+        {
+            Debug.WriteLine("KonsumKreditVerwaltung - BildungsAngabenLaden");
+            Debug.Indent();
+
+            List<Schulabschluss> alleAbschlüsse = null;
+
+            try
+            {
+                using (var context = new dbOnlineKreditLAPEntities1())
+                {
+                    alleAbschlüsse = context.AlleSchulabschlüsse.ToList();
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("Fehler in BildungsAngabenLaden");
+                Debug.Indent();
+                Debug.WriteLine(ex.Message);
+                Debug.Unindent();
+                Debugger.Break();
+            }
+
+            Debug.Unindent();
+            return alleAbschlüsse;
+        }
+
+        public static List<FamilienStandAngabe> FamilienStandAngabenLaden()
+        {
+            Debug.WriteLine("KonsumKreditVerwaltung - FamilienStandAngabenLaden");
+            Debug.Indent();
+
+            List<FamilienStandAngabe> alleFamilienStandsAngaben = null;
+
+            try
+            {
+                using (var context = new dbOnlineKreditLAPEntities1())
+                {
+                    alleFamilienStandsAngaben = context.AlleFamilienStandAngaben.ToList();
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("Fehler in FamilienStandAngabenLaden");
+                Debug.Indent();
+                Debug.WriteLine(ex.Message);
+                Debug.Unindent();
+                Debugger.Break();
+            }
+
+            Debug.Unindent();
+            return alleFamilienStandsAngaben;
+        }
+
+        public static List<Land> LaenderLaden()
+        {
+            Debug.WriteLine("KonsumKreditVerwaltung - LaenderLaden");
+            Debug.Indent();
+
+            List<Land> alleLänder = null;
+
+            try
+            {
+                using (var context = new dbOnlineKreditLAPEntities1())
+                {
+                    alleLänder = context.AlleLänder.ToList();
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("Fehler in LaenderLaden");
+                Debug.Indent();
+                Debug.WriteLine(ex.Message);
+                Debug.Unindent();
+                Debugger.Break();
+            }
+
+            Debug.Unindent();
+            return alleLänder;
+        }
+
+        public static List<Wohnart> WohnartenLaden()
+        {
+            Debug.WriteLine("KonsumKreditVerwaltung - WohnartenLaden");
+            Debug.Indent();
+
+            List<Wohnart> alleWohnarten = null;
+
+            try
+            {
+                using (var context = new dbOnlineKreditLAPEntities1())
+                {
+                    alleWohnarten = context.AlleWohnarten.ToList();
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("Fehler in WohnartenLaden");
+                Debug.Indent();
+                Debug.WriteLine(ex.Message);
+                Debug.Unindent();
+                Debugger.Break();
+            }
+
+            Debug.Unindent();
+            return alleWohnarten;
+        }
+
+        public static List<IdentifikationsArt> IdentifikiationsAngabenLaden()
+        {
+            Debug.WriteLine("KonsumKreditVerwaltung - IdentifikiationsAngabenLaden");
+            Debug.Indent();
+
+            List<IdentifikationsArt> alleIdentifikationsArten = null;
+
+            try
+            {
+                using (var context = new dbOnlineKreditLAPEntities1())
+                {
+                    alleIdentifikationsArten = context.AlleIdentifikationsArten.ToList();
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("Fehler in IdentifikiationsAngabenLaden");
+                Debug.Indent();
+                Debug.WriteLine(ex.Message);
+                Debug.Unindent();
+                Debugger.Break();
+            }
+
+            Debug.Unindent();
+            return alleIdentifikationsArten;
+        }
+
+        public static List<Titel> TitelLaden()
+        {
+            Debug.WriteLine("KonsumKreditVerwaltung - TitelLaden");
+            Debug.Indent();
+
+            List<Titel> alleTitel = null;
+
+            try
+            {
+                using (var context = new dbOnlineKreditLAPEntities1())
+                {
+                    alleTitel = context.AlleTitel.ToList();
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("Fehler in TitelLaden");
+                Debug.Indent();
+                Debug.WriteLine(ex.Message);
+                Debug.Unindent();
+                Debugger.Break();
+            }
+
+            Debug.Unindent();
+            return alleTitel;
+        }
+
+        public static bool PersönlicheDatenSpeichern(int? idTitel, string geschlecht, DateTime geburtsDatum, string vorname, string nachname, int? idTitelNachstehend, int idBildung, int idFamilienstand, int idIdentifikationsart, string identifikationsNummer, string idStaatsbuergerschaft, int idWohnart, int idKunde)
+        {
+            Debug.WriteLine("KonsumKreditVerwaltung - PersönlicheDatenSpeichern");
+            Debug.Indent();
+
+            bool erfolgreich = false;
+
+            try
+            {
+                using (var context = new dbOnlineKreditLAPEntities1())
+                {
+
+                    /// speichere zum Kunden die Angaben
+                    Kunde aktKunde = context.AlleKunden.Where(x => x.ID == idKunde).FirstOrDefault();
+
+                    if (aktKunde != null)
+                    {
+                        aktKunde.Vorname = vorname;
+                        aktKunde.Nachname = nachname;
+                        aktKunde.FKFamilienstand = idFamilienstand;
+                        aktKunde.FKSchulabschluss = idBildung;
+                        aktKunde.FKStaatsangehoerigkeit = idStaatsbuergerschaft;
+                        aktKunde.FKTitel = idTitel;
+                        aktKunde.FKIdentifikationsArt = idIdentifikationsart;
+                        aktKunde.IdentifikationsNummer = identifikationsNummer;
+                        aktKunde.Geschlecht = geschlecht;
+                        aktKunde.FKWohnart = idWohnart;
+                    }
+
+                    int anzahlZeilenBetroffen = context.SaveChanges();
+                    erfolgreich = anzahlZeilenBetroffen >= 1;
+                    Debug.WriteLine($"{anzahlZeilenBetroffen} PersönlicheDaten gespeichert!");
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("Fehler in PersönlicheDatenSpeichern");
+                Debug.Indent();
+                Debug.WriteLine(ex.Message);
+                Debug.Unindent();
+                Debugger.Break();
+            }
+
+            Debug.Unindent();
+            return erfolgreich;
+        }
+
+        public static bool ArbeitgeberAngabenSpeichern(string firmenName, int idBeschäftigungsArt, int idBranche, string beschäftigtSeit, int idKunde)
+        {
+            Debug.WriteLine("KonsumKreditVerwaltung - ArbeitgeberAngabenSpeichern");
+            Debug.Indent();
+
+            bool erfolgreich = false;
+
+            try
+            {
+                using (var context = new dbOnlineKreditLAPEntities1())
+                {
+
+                    /// speichere zum Kunden die Angaben
+                    Kunde aktKunde = context.AlleKunden.Where(x => x.ID == idKunde).FirstOrDefault();
+
+                    if (aktKunde != null)
+                    {
+                        Arbeitgeber neuerArbeitgeber = new Arbeitgeber()
+                        {
+                            BeschaeftigtSeit = DateTime.Parse(beschäftigtSeit),
+                            FKBranche = idBranche,
+                            FKBeschaeftigungsArt = idBeschäftigungsArt,
+                            Firma = firmenName
+                        };
+                        aktKunde.Arbeitgeber = neuerArbeitgeber;
+                    }
+
+                    int anzahlZeilenBetroffen = context.SaveChanges();
+                    erfolgreich = anzahlZeilenBetroffen >= 1;
+                    Debug.WriteLine($"{anzahlZeilenBetroffen} ArbeitgeberDaten gespeichert!");
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("Fehler in ArbeitgeberAngabenSpeichern");
+                Debug.Indent();
+                Debug.WriteLine(ex.Message);
+                Debug.Unindent();
+                Debugger.Break();
+            }
+
+            Debug.Unindent();
+            return erfolgreich;
         }
     }
 }
